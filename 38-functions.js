28fe7cbc93485a99a9441e84cbe69ffe4f8ccec4
Teja0307 <119520279+Teja0307@users.noreply.github.com>
2022-12-31 21:21:18 +0530

    Add files via upload

---
+++ 38-functions.js
@@ -0,0 +1,26 @@
+console.log("Hi I am teja");
+
+
+function fun(std,message){
+    console.log(message+" "+std);
+    console.log(std + " is a student");
+}
+let name1="guru";
+let name2="sai";
+let name3="venkata";
+let name4="teja";
+let msg="hi";
+fun(name1,msg);
+fun(name2,msg);
+fun(name3,msg);
+fun(name4,msg);
+
+
+
+function sum(a,b,c){
+    let d=a+b+c;
+    return d;
+}
+
+let returnvalue=sum(1,2,3);
+console.log(returnvalue);
